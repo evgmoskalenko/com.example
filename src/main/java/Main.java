48edd586eac697c95082f97c075fac0fdddaa263
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2016-05-25 01:16:16 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Integer a = 128;
+        Integer b = 128;
+
+        boolean result = a == b;
+
+        System.out.println(result);
+
+    }
+
+}
